c17a219faaabbc8a99bd1e748aa06b40223c8179
filip2cz <filip@fkomarek.eu>
2024-01-14 17:28:29 +0100

    ADD: propably valid upstream param

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "henshouse/henshouse:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
